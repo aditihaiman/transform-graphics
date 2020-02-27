2091ac946c4d93f15223b514a3b28ce1d09b87f5
aditihaiman <ahaiman00@stuy.edu>
2020-02-27 15:15:55 -0500

    testing and debugging

--- main.py
+++ main.py
@@ -6,12 +6,41 @@ import math
 import random
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
+color = [ 255, 255, 255 ]
 edges = []
 transform = ident(new_matrix())
 
+add_edge(edges, 0, 0, 0, 100, 0, 0)
+add_edge(edges, 100, 0, 0, 100, 100, 0)
+add_edge(edges, 100, 100, 0, 0, 100, 0)
+add_edge(edges, 0, 100, 0, 0, 0, 0)
+add_edge(edges, 0, 0, 100, 100, 0, 100)
+add_edge(edges, 100, 0, 100, 100, 100, 100)
+add_edge(edges, 100, 100, 100, 0, 100, 100)
+add_edge(edges, 0, 100, 100, 0, 0, 100)
+add_edge(edges, 0, 0, 0, 0, 0, 100)
+add_edge(edges, 0, 100, 0, 0, 100, 100)
+add_edge(edges, 100, 100, 0, 100, 100, 100)
+add_edge(edges, 100, 0, 0, 100, 0, 100)
 
-#display(screen)
+transform = make_scale(2, 2, 2)
+print_matrix(transform)
+print("\n")
+draw_lines(edges, screen, color)
+
+matrix_mult(make_rotZ(45), transform)
+#print_matrix(transform)
+#print("\n")
+matrix_mult(make_translate(50, 50, 50), transform)
+print_matrix(transform)
+
+
+
+matrix_mult(transform, edges)
+draw_lines(edges, screen, color)
+
+
+display(screen)
 #save_ppm(screen, 'binary.ppm')
 #save_ppm_ascii(screen, 'ascii.ppm')
 #save_extension(screen, 'img.png')
